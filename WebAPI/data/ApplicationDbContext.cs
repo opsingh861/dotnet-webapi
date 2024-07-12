55f98054d09a6085f64c1583bd32e0c603ba28da
Aditya Dhanraj <opsingh861@gmail.com>
2024-07-12 10:01:37 +0530

    Test API

---
+++ WebAPI/data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models.Entities;
+
+namespace WebAPI.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
